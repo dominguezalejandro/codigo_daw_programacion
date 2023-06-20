36fa2955459bc8ad2d63b075071307bd0cbfc52d
Alejandro Domínguez <133596732+dominguezalejandro@users.noreply.github.com>
2023-06-20 10:12:16 +0200

    Update and rename hola.md to hola.js

--- codigo/hola.js
+++ codigo/hola.js
@@ -1,8 +1,9 @@
-`public class MiClase {
+public class MiClase {
     public static void main(String[] args) {
        // Inicio del bloque de código
         System.out.println("Este es un bloque de código");
         System.out.println("con múltiples instrucciones.");
         // Fin del bloque de código
     }
-}`
+}
+
